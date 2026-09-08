b9a1aad60df0d60501632f602c5ce93467b76775
Joaquín <joaquin.p.olivera@gmail.com>
2026-09-08 16:31:59 -0300

    Remove old com.joaquindev.triptrace package files

--- app/src/main/java/com/joaquindev/triptrace/ui/navigation/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.joaquindev.triptrace.ui.navigation
-
-object Routes {
-    const val LOGIN = "login"
-    const val TRIPS = "trips"
-    const val HISTORY = "history"
-    const val CREATE_TRIP = "create_trip"
-    const val ACTIVE_TRIP = "active_trip"
-}
