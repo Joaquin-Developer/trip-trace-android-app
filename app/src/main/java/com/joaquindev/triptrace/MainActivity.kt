be80932372d1dc2372d738fc1e53344804030523
Joaquín <joaquin.p.olivera@gmail.com>
2026-09-08 16:23:20 -0300

    Wire MainActivity to TripTraceNavHost

--- app/src/main/java/com/joaquindev/triptrace/MainActivity.kt
+++ app/src/main/java/com/joaquindev/triptrace/MainActivity.kt
@@ -4,14 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.joaquindev.triptrace.ui.navigation.TripTraceNavHost
 import com.joaquindev.triptrace.ui.theme.TripTraceTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,25 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TripTraceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(modifier = Modifier.padding(innerPadding))
-                }
+                TripTraceNavHost()
             }
         }
-    }
-}
-
-@Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
-    Surface(modifier = modifier) {
-        Text(text = "TripTrace")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview() {
-    TripTraceTheme {
-        HomeScreen()
     }
 }
